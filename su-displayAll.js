b12d6f5e3853eca48a9c69cf9923f848aea6c359
Truong-N <truong36gm@gmail.com>
2023-02-18 15:50:20 -0500

    remove all files sudoku game written with js

--- su-displayAll.js
+++
@@ -1,8 +0,0 @@
-const displayAll = () => {
-  for (let row = 0; row < 9; row++) {
-    for (let col = 0; col < 9; col++) {
-      document.getElementById(`sr${row}c${col}`).style.display = "block";
-      document.getElementById(`ur${row}c${col}`).style.display = "block";
-    }
-  }
-};
